bb418582d32442846ff804a77bf11581047ee777
Tom C <tom.carlier@epitech.eu>
2024-04-18 12:24:31 +0200

    [feat] -->  add the || command separator

--- src/shell/shell_process_commands.c
+++ src/shell/shell_process_commands.c
@@ -15,6 +15,8 @@ static int process_command(command_t *cmd, shell_t *shell, command_t **curr)
         return shell_process_command(shell, cmd);
     if (command_get_type(cmd) == AND)
         return shell_process_and(shell, cmd, curr);
+    if (command_get_type(cmd) == OR)
+        return shell_process_or(shell, cmd, curr);
     return 0;
 }
 
