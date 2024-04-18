8a63b17f481b643758eeee9f6cd5e97ec21a2a20
EmmaFrx <emma.farineaux@epitech.eu>
2024-04-18 12:55:18 +0200

    [fix] --> setting the builtin builtins when the terminal is too small

--- src/builtin/builtins/builtin_do_builtins.c
+++ src/builtin/builtins/builtin_do_builtins.c
@@ -30,6 +30,22 @@ int get_num_col(int num_col, int num_builtins,
     return num_col;
 }
 
+int display_builtins_little_terminal(void)
+{
+    for (int i = 0; builtins[i].name != NULL; i++) {
+        printf("%s ", builtins[i].name);
+    }
+    printf("\n");
+    return 0;
+}
+
+int if_terminal_to_little(shell_t *shell)
+{
+    display_builtins_little_terminal();
+    shell_set_code(shell, 1);
+    return 0;
+}
+
 int builtin_do_builtins(command_t *command, shell_t *shell)
 {
     int num_builtins = sizeof(builtins) / sizeof(builtins[0]);
@@ -42,6 +58,8 @@ int builtin_do_builtins(command_t *command, shell_t *shell)
 
     ioctl(0, TIOCGWINSZ, &w);
     size_terminal = w.ws_col;
+    if (size_terminal == 0)
+        return if_terminal_to_little(shell);
     num_col = size_terminal / min_col_width;
     num_col = get_num_col(num_col, num_builtins, max_col_width, size_terminal);
     col_width = size_terminal / num_col;
