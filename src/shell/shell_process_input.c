de1043b4aaac400f553353d65ac91f35f95faafc
EmmaFrx <emma.farineaux@epitech.eu>
2024-04-29 12:14:45 +0200

    [refactor] -->  modified shell_process_input function to accommodate variables

--- src/shell/shell_process_input.c
+++ src/shell/shell_process_input.c
@@ -7,10 +7,16 @@
 
 #include "my.h"
 #include <stdio.h>
+#include "variables.h"
+#include <stdlib.h>
 
 int shell_process_input(shell_t *shell, char const *input)
 {
-    shell_parse_input(shell, input);
+    char *new_input = get_if_variable(input, shell);
+
+    if (new_input == NULL)
+        return 0;
+    shell_parse_input(shell, new_input);
     if (shell_process_cmds_error(shell) == 1)
         return 0;
     shell_process_commands(shell);
