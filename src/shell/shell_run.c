96be7bb1b96b93705d43748d6e1ff72a68bc4158
Antoine Orange <antoine.orange@epitech.eu>
2024-05-03 12:44:10 +0200

    [docs] function doc of shell_run.c

--- src/shell/shell_run.c
+++ src/shell/shell_run.c
@@ -9,6 +9,18 @@
 #include <stdio.h>
 #include <string.h>
 
+////////////////////////////////////////////////////////////
+// int shell_run(shell_t *shell)
+//
+// shell -> structure that contains all the data of the shell
+//
+// This function launches the loop of the shell, and executes the prompt,
+// input detection and execution and launches all the processes.
+//
+// RETURN VALUE : int that represent the status of the execution
+// the program return 84 when an error occurs, and return 0
+// if all works correctly.
+////////////////////////////////////////////////////////////
 int shell_run(shell_t *shell)
 {
     char *line = NULL;
