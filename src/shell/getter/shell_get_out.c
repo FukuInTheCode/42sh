dc6de324cd0e36e9e23e2b829a92349355f28714
Antoine Orange <antoine.orange@epitech.eu>
2024-05-06 00:35:58 +0200

    [docs] function doc of shell_get_out.c

--- src/shell/getter/shell_get_out.c
+++ src/shell/getter/shell_get_out.c
@@ -7,6 +7,15 @@
 
 #include "my.h"
 
+////////////////////////////////////////////////////////////
+// int shell_get_out(shell_t *shell)
+//
+// shell -> structure that contains all the data of the shell
+//
+// This function returns the shell output value.
+//
+// RETURN VALUE : int that represent the output of the shell
+////////////////////////////////////////////////////////////
 int shell_get_out(shell_t *shell)
 {
     return shell->out;
