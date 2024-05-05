123b01b8618f5dc245d8a15f1bfb0b555bea31bc
Antoine Orange <antoine.orange@epitech.eu>
2024-05-06 00:34:15 +0200

    [docs] function doc of command_destroy.c

--- src/command/command_destroy.c
+++ src/command/command_destroy.c
@@ -8,6 +8,17 @@
 #include "my.h"
 #include <stdlib.h>
 
+
+////////////////////////////////////////////////////////////
+// int command_destroy(command_t *command)
+//
+// command -> structure that contains all the data of the
+// current command
+//
+// This function frees the command passed as parameters.
+//
+// RETURN VALUE : int that is always 0
+////////////////////////////////////////////////////////////
 int command_destroy(command_t *command)
 {
     if (!command)
