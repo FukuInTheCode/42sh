da40073aad9fe08e26e091c3f571f8420d16ed34
Antoine Orange <antoine.orange@epitech.eu>
2024-05-07 00:58:37 +0200

    [docs] function doc of parser_set_dblquote.c

--- src/parser/setter/parser_set_dblquote.c
+++ src/parser/setter/parser_set_dblquote.c
@@ -7,6 +7,16 @@
 
 #include "my.h"
 
+////////////////////////////////////////////////////////////
+// int parser_set_dblquote(parser_t *parser, bool quote)
+//
+// parser -> all the data of the parser
+// quote -> status of double quote presence
+//
+// This function sets the double quote status of the parser.
+//
+// RETURN VALUE : int that is always 0
+////////////////////////////////////////////////////////////
 int parser_set_dblquote(parser_t *parser, bool quote)
 {
     parser->in_double_quote = quote;
