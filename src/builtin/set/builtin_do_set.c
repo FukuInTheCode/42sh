99dc7a942fd8197b51703114a1bab4ae8c90bfd9
Tom C <tom.carlier@epitech.eu>
2024-05-07 17:35:57 +0200

    [feat] -->  add the no arguments set feature

--- src/builtin/set/builtin_do_set.c
+++ src/builtin/set/builtin_do_set.c
@@ -5,6 +5,7 @@
 ** builtin_do_set.c
 */
 
+#include "command.h"
 #include "my.h"
 #include "variables.h"
 #include "builtins.h"
@@ -23,6 +24,17 @@ int get_if_error(shell_t *shell, char *assigned)
     return 0;
 }
 
+static int print_vars(shell_t *shell)
+{
+    for (variables_t *tmp = shell->head; tmp; tmp = tmp->next) {
+        printf("%s\t", tmp->assigned);
+        if (tmp->to_doux)
+            printf("%s", tmp->to_doux);
+        printf("\n");
+    }
+    return 0;
+}
+
 int builtin_do_set(command_t *command, shell_t *shell)
 {
     char **argv = NULL;
@@ -32,6 +44,8 @@ int builtin_do_set(command_t *command, shell_t *shell)
     if (command == NULL || shell == NULL)
         return 84;
     argv = command_get_argv(command);
+    if (command_get_argc(command) == 1)
+        return print_vars(shell);
     for (int i = 1; i < command->argc; i++) {
         assigned = NULL;
         need_to_do = NULL;
