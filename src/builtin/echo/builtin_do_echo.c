4e7c980a395f0ddb24411b6dbe0aa079756df367
Tom C <tom.carlier@epitech.eu>
2024-05-10 16:31:55 +0200

    [feat] -->  add the echo builtin

---
+++ src/builtin/echo/builtin_do_echo.c
@@ -0,0 +1,30 @@
+/*
+** EPITECH PROJECT, 2024
+** builtin_do_echo.c
+** File description:
+** builtin_do_echo.c
+*/
+
+#include "builtins.h"
+#include <stdbool.h>
+#include <string.h>
+#include <stdio.h>
+
+int builtin_do_echo(command_t *command, shell_t *shell)
+{
+    bool print_new_line = true;
+
+    for (int i = 0; i < command->argc; i++) {
+        if (!strcmp("-n", command->argv[i])) {
+            print_new_line = false;
+            continue;
+        }
+        if (i != 0)
+            printf(" ");
+        printf("%s", command->argv[i]);
+    }
+    if (print_new_line)
+        printf("\n");
+    shell_set_code(shell, 0);
+    return 0;
+}
