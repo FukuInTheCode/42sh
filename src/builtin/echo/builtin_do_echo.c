8dc547e8fb0ea3a474e8ca3056f55fe3a76d00e1
Tom C <tom.carlier@epitech.eu>
2024-05-10 16:52:26 +0200

    [fix] --> fix the echo print of the space

--- src/builtin/echo/builtin_do_echo.c
+++ src/builtin/echo/builtin_do_echo.c
@@ -13,15 +13,17 @@
 int builtin_do_echo(command_t *command, shell_t *shell)
 {
     bool print_new_line = true;
+    bool first = true;
 
     for (int i = 1; i < command->argc; i++) {
         if (!strcmp("-n", command->argv[i])) {
             print_new_line = false;
             continue;
         }
-        if (i != 0)
+        if (!first)
             printf(" ");
         printf("%s", command->argv[i]);
+        first = false;
     }
     if (print_new_line)
         printf("\n");
