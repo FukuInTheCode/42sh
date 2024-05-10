66f2930973cc30ede95e969fce3fa587c4c59c1e
Antoine Orange <antoine.orange@epitech.eu>
2024-05-10 18:37:42 +0200

    [docs] function doc of shell_process_command.c

--- src/shell/shell_process_command.c
+++ src/shell/shell_process_command.c
@@ -7,6 +7,16 @@
 
 #include "my.h"
 
+////////////////////////////////////////////////////////////
+// int shell_process_command(shell_t *shell, command_t *cmd)
+//
+// shell -> structure of the shell
+// cmd -> structure of the command
+//
+// This function processes one command in the shell.
+//
+// RETURN VALUE : int that is always 0
+////////////////////////////////////////////////////////////
 int shell_process_command(shell_t *shell, command_t *cmd)
 {
     if (command_exec(cmd, (void *)shell))
