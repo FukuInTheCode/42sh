f4c5d6e04c6fe47e21c4b0b8becbde508d33b435
EmmaFrx <emma.farineaux@epitech.eu>
2024-05-10 16:32:20 +0200

    [refactor] -->  add the prototype of the function builtin_do_alias

--- include/builtins.h
+++ include/builtins.h
@@ -33,6 +33,7 @@ int builtin_do_builtins(command_t *, shell_t *);
 int builtin_do_which(command_t *, shell_t *);
 int builtin_do_where(command_t *, shell_t *);
 int builtin_do_at(command_t *, shell_t *);
+int builtin_do_alias(command_t *, shell_t *);
 
 int builtin_handle_varname_error(shell_t *, char const *);
 int builtin_handle_varfirst_error(shell_t *, char const *);
@@ -59,6 +60,7 @@ static builtin_t const builtins[] = {
     {":", builtin_do_colon},
     {"builtins", builtin_do_builtins},
     {"@", builtin_do_at},
+    {"alias", builtin_do_at},
     {"cd", builtin_do_cd},
     {"chdir", builtin_do_cd},
     {"exit", builtin_do_exit},
