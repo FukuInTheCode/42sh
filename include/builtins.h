db68e249058da3629ba17b7af0fdae1c67c3fd53
EmmaFrx <emma.farineaux@epitech.eu>
2024-05-10 18:08:50 +0200

    [refactor] --> added the prototype for the builtin jobs

--- include/builtins.h
+++ include/builtins.h
@@ -62,6 +62,7 @@ int builtin_do_goto(command_t *, shell_t *);
 int builtin_do_hashstat(command_t *, shell_t *);
 int builtin_do_hup(command_t *, shell_t *);
 int builtin_do_if(command_t *, shell_t *);
+int builtin_do_jobs(command_t *, shell_t *);
 
 int builtin_handle_varname_error(shell_t *, char const *);
 int builtin_handle_varfirst_error(shell_t *, char const *);
@@ -124,6 +125,7 @@ static builtin_t const builtins[] = {
     {"history", builtin_do_history},
     {"hup", builtin_do_hup},
     {"if", builtin_do_if},
+    {"jobs", builtin_do_jobs},
     {"printenv", builtin_do_printenv},
     {"repeat", builtin_do_repeat},
     {"set", builtin_do_set},
