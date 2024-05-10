5cc4a5c3eec0e281f0a8a17b36c3769bdf862cbe
Antoine Orange <antoine.orange@epitech.eu>
2024-05-10 18:54:46 +0200

    [docs] function doc of shell_get_history_id.c

--- src/shell/getter/shell_get_history_id.c
+++ src/shell/getter/shell_get_history_id.c
@@ -7,6 +7,15 @@
 
 #include "my.h"
 
+////////////////////////////////////////////////////////////
+// int shell_get_history_id(shell_t *shell)
+//
+// shell -> structure that contains all the data of the shell
+//
+// This function returns the shell history_id value.
+//
+// RETURN VALUE : int that represent the history_id of the shell
+////////////////////////////////////////////////////////////
 int shell_get_history_id(shell_t *shell)
 {
     return shell->history_id;
