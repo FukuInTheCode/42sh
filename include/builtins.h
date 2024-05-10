326a06c51482e1911fced75752c63eaa7f417bf4
EmmaFrx <emma.farineaux@epitech.eu>
2024-05-10 18:10:57 +0200

    [refactor] --> added the prototype for the builtin login

--- include/builtins.h
+++ include/builtins.h
@@ -65,6 +65,7 @@ int builtin_do_if(command_t *, shell_t *);
 int builtin_do_jobs(command_t *, shell_t *);
 int builtin_do_limit(command_t *, shell_t *);
 int builtin_do_log(command_t *, shell_t *);
+int builtin_do_login(command_t *, shell_t *);
 
 int builtin_handle_varname_error(shell_t *, char const *);
 int builtin_handle_varfirst_error(shell_t *, char const *);
@@ -130,6 +131,7 @@ static builtin_t const builtins[] = {
     {"jobs", builtin_do_jobs},
     {"limit", builtin_do_limit},
     {"log", builtin_do_log},
+    {"login", builtin_do_login},
     {"printenv", builtin_do_printenv},
     {"repeat", builtin_do_repeat},
     {"set", builtin_do_set},
