30e253e85d1e2317bf23e75b57852b6693dbbcdf
EmmaFrx <emma.farineaux@epitech.eu>
2024-05-10 17:35:14 +0200

    [refactor] -->  added the prototype of the function builtin_do_end

--- include/builtins.h
+++ include/builtins.h
@@ -49,6 +49,7 @@ int builtin_do_default(command_t *, shell_t *);
 int builtin_do_dirs(command_t *, shell_t *);
 int builtin_do_echotc(command_t *, shell_t *);
 int builtin_do_else(command_t *, shell_t *);
+int builtin_do_end(command_t *, shell_t *);
 
 int builtin_handle_varname_error(shell_t *, char const *);
 int builtin_handle_varfirst_error(shell_t *, char const *);
@@ -96,6 +97,7 @@ static builtin_t const builtins[] = {
     {"dirs", builtin_do_dirs},
     {"echotc", builtin_do_echotc},
     {"else", builtin_do_else},
+    {"end", builtin_do_else},
     {"exit", builtin_do_exit},
     {"history", builtin_do_history},
     {"printenv", builtin_do_printenv},
