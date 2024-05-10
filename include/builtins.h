b4d277730d8d09ca9e71c69ec47a0e30c9deaaf7
EmmaFrx <emma.farineaux@epitech.eu>
2024-05-10 18:10:12 +0200

    [refactor] --> added the prototype for the builtin log

--- include/builtins.h
+++ include/builtins.h
@@ -64,6 +64,7 @@ int builtin_do_hup(command_t *, shell_t *);
 int builtin_do_if(command_t *, shell_t *);
 int builtin_do_jobs(command_t *, shell_t *);
 int builtin_do_limit(command_t *, shell_t *);
+int builtin_do_log(command_t *, shell_t *);
 
 int builtin_handle_varname_error(shell_t *, char const *);
 int builtin_handle_varfirst_error(shell_t *, char const *);
@@ -128,6 +129,7 @@ static builtin_t const builtins[] = {
     {"if", builtin_do_if},
     {"jobs", builtin_do_jobs},
     {"limit", builtin_do_limit},
+    {"log", builtin_do_log},
     {"printenv", builtin_do_printenv},
     {"repeat", builtin_do_repeat},
     {"set", builtin_do_set},
